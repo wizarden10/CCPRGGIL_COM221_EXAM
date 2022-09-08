90a36f8061f53dc95319883fcd59828c46201838
Wildelyn Arriesgado <111488325+wizarden10@users.noreply.github.com>
2022-09-08 14:40:11 +0800

    Create App.java

---
+++ Exam_1/App.java
@@ -0,0 +1,24 @@
+public class App {
+    public static void main(String[] args) {
+        
+          Student pretty = new Student();
+
+                pretty.surname = "Arriesgado";
+                pretty.firstname = "Wildelyn";
+                pretty.middleInitial = 'S';
+                pretty.dateOfBirth = 11/10/2001;
+                pretty.studentNumber = 2022101336;
+                pretty.emailAddress = "arriesgadow@students.national-u.edu.ph";
+                pretty.IamAwesome = true;
+                pretty.sayMyStudentNumber();
+                pretty.sayMyEmailAddress();
+                pretty.amIAwesome();
+
+    
+
+
+
+
+    
+    }
+}
